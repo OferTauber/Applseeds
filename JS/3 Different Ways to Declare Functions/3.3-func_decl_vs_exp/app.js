6406e026a58ea183f89d5d6d5a9ae805ae5885da
Ofer Tauber <ofertauber@gmail.com>
2022-04-06 12:08:43 +0300

    3.3 function declarations vs expressions

--- JS/3 Different Ways to Declare Functions/3.3-func_decl_vs_exp/app.js
+++ JS/3 Different Ways to Declare Functions/3.3-func_decl_vs_exp/app.js
@@ -1,19 +1,56 @@
-// 1. Write a function called 'describeCountry' which takes three parameters:
-// 'country', 'population' and 'capitalCity'.
-// Based on this input, the function returns a string with this
-// format: 'Finland has 6 million people and its capital city is Helsinki'
+// 1. The world population is 7900 million people. Create a
+// function declaration called 'percentageOfWorld1' which
+// receives a 'population' value, and returns the percentage of
+// the world population that the given population represents.
+// For example, China has 1441 million people, so it's about
+// 18.2% of the world population.
 
-function describeCountry(country, population_millions, capitalCity) {
-  return `${country} has ${population_millions} million people and its capital city is ${capitalCity}`;
-}
+// 2. To calculate the percentage, divide the given 'population'
+// value by 7900 and then multiply by 100
+const world_pop = 7900;
 
-// 2. Call this function 3 times, with input data for 3 different
-// countries. Store the returned values in 3 different variables,
-// and log them to the console.
-const israel = describeCountry('Israel', 10, 'Jerusalem');
-const australia = describeCountry('Australia', 35, 'Canberra');
-const india = describeCountry('India', 1400, 'New Delhi');
+percentageOfWorld1 = (population) => (population / world_pop) * 100;
 
-console.log(israel);
-console.log(australia);
-console.log(india);
+displayFloor = (num) => Math.floor(num * 100) / 100;
+// 3. Call 'percentageOfWorld1' for 3 populations of countries
+// of your choice, store the results into variables, and log
+// them to the console
+
+const china_pop = percentageOfWorld1(1441);
+const israel_pop = percentageOfWorld1(9);
+const egypt_pop = percentageOfWorld1(105);
+
+console.log(
+  `China's population makes up ${displayFloor(
+    china_pop
+  )}% of the world's population`
+);
+console.log(
+  `Israels's population makes up ${displayFloor(
+    israel_pop
+  )}% of the world's population`
+);
+console.log(
+  `Egypt's population makes up ${displayFloor(
+    egypt_pop
+  )}% of the world's population`
+);
+
+// 4. Create a function expression which does the exact same
+// thing, called 'percentageOfWorld2', and also call it with 3
+// country populations (can be the same populations)
+const percentageOfWorld2 = (population) => (population / world_pop) * 100;
+
+const india_pop = displayFloor(percentageOfWorld1(1352));
+const nigeria_pop = displayFloor(percentageOfWorld1(211));
+const hong_kong_pop = displayFloor(percentageOfWorld1(7));
+
+console.log(
+  `India's population makes up ${india_pop}% of the world's population`
+);
+console.log(
+  `Nigeria's population makes up ${nigeria_pop}% of the world's population`
+);
+console.log(
+  `Hong Kong's population makes up ${hong_kong_pop}% of the world's population`
+);
