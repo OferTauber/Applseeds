c6a307cbbe9ae95148c32d0041fdfe1845bbbbf6
Ofer Tauber <ofertauber@gmail.com>
2022-04-06 12:56:53 +0300

    4.1 number of siblings

--- JS/4 Order Of Operations, If Statements, Truthy and Falsy/4.1-number of siblings/app.js
+++ JS/4 Order Of Operations, If Statements, Truthy and Falsy/4.1-number of siblings/app.js
@@ -1,19 +1,46 @@
-// 1. Write a function called 'describeCountry' which takes three parameters:
-// 'country', 'population' and 'capitalCity'.
-// Based on this input, the function returns a string with this
-// format: 'Finland has 6 million people and its capital city is Helsinki'
+// 1. Declare a variable 'numSiblings' based on a prompt input
+// like this:
+// prompt('How many siblings do you have?');
 
-function describeCountry(country, population_millions, capitalCity) {
-  return `${country} has ${population_millions} million people and its capital city is ${capitalCity}`;
+numSiblings = prompt('How many siblings do you have?');
+
+// 2. If you have one sibling, log to the console '1 sibling!'
+// (use loose equality == for now)
+
+//1 3. Use an else-if block to log 'More than 1 sibling' in case
+// 'numSiblings' is greater than 1
+
+// 4. Use an else block to log 'No siblings' (this block will be
+// executed when 'numSiblings' is 0 or any other value)
+
+/*
+if (numSiblings == 1) {
+  console.log(`1 sibling`);
+} else if (numSiblings < 1) {
+  console.log(`No sibling`);
+} else {
+  console.log(`More than 1 sibling`);
 }
+*/
 
-// 2. Call this function 3 times, with input data for 3 different
-// countries. Store the returned values in 3 different variables,
-// and log them to the console.
-const israel = describeCountry('Israel', 10, 'Jerusalem');
-const australia = describeCountry('Australia', 35, 'Canberra');
-const india = describeCountry('India', 1400, 'New Delhi');
+// 5. Test the code with different values of 'numSiblings',
+// including 1 and 0.
 
-console.log(israel);
-console.log(australia);
-console.log(india);
+// 6. Change == to ===, and test the code again, with the
+// same values of 'numSiblings'. Notice what happens when
+// there is exactly 1 sibling! Why is this happening?
+
+if (~~numSiblings === 1) {
+  console.log(`1 sibling`);
+} else if (1 * numSiblings < 1) {
+  console.log(`No sibling`);
+} else {
+  console.log(`More than 1 sibling`);
+}
+// 7. Finally, convert 'numSibling' to a number, and watch
+// what happens now when you input 1.
+
+// 8. Reflect on why we should use the === operator and
+// type conversion in this situation.
+
+// Because JS allows us to implement any type of data in a variable - using the '===' operator  will help us detect incorrect placement of data to a variable, especially cases of comparing a variable to "true" or "false". In case we have replaced the value of a Boolean variable, and we check if it is not true using '==' - there is a risk of undefined behavior, while using '===' will always have a stable behavior
