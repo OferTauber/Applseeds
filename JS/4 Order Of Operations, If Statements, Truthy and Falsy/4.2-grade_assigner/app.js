3f765ddc9014c4450d9bb93f2f6946688c6ba0a2
Ofer Tauber <ofertauber@gmail.com>
2022-04-06 15:05:53 +0300

    Basketball score

--- JS/4 Order Of Operations, If Statements, Truthy and Falsy/4.2-grade_assigner/app.js
+++ JS/4 Order Of Operations, If Statements, Truthy and Falsy/4.2-grade_assigner/app.js
@@ -1,19 +1,25 @@
-// 1. Write a function called 'describeCountry' which takes three parameters:
-// 'country', 'population' and 'capitalCity'.
-// Based on this input, the function returns a string with this
-// format: 'Finland has 6 million people and its capital city is Helsinki'
+// Create a function that takes 1 argument, a number score.
+// Returns either “A”, ”B”, “C”, “D”, “F”.
+// score of:
+// 0-64 is a “F”
+// 65-69 is a “D”
+// 70-79 is a “C”
+// 80-89 is a “B”
+// 90-100 is an “A
 
-function describeCountry(country, population_millions, capitalCity) {
-  return `${country} has ${population_millions} million people and its capital city is ${capitalCity}`;
-}
-
-// 2. Call this function 3 times, with input data for 3 different
-// countries. Store the returned values in 3 different variables,
-// and log them to the console.
-const israel = describeCountry('Israel', 10, 'Jerusalem');
-const australia = describeCountry('Australia', 35, 'Canberra');
-const india = describeCountry('India', 1400, 'New Delhi');
-
-console.log(israel);
-console.log(australia);
-console.log(india);
+const gradeAssinger = (grade) => {
+  const num_grade = 1 * grade;
+  let to_return = '';
+  if (num_grade < 65) {
+    to_return = 'F';
+  } else if (num_grade < 70) {
+    to_return = 'D';
+  } else if (num_grade < 80) {
+    to_return = 'C';
+  } else if (num_grade < 90) {
+    to_return = 'B';
+  } else {
+    to_return = 'A';
+  }
+  return to_return;
+};
--- JS/4 Order Of Operations, If Statements, Truthy and Falsy/4.3-basketball/app.js
+++ JS/4 Order Of Operations, If Statements, Truthy and Falsy/4.3-basketball/app.js
@@ -1,19 +1,66 @@
-// 1. Write a function called 'describeCountry' which takes three parameters:
-// 'country', 'population' and 'capitalCity'.
-// Based on this input, the function returns a string with this
-// format: 'Finland has 6 million people and its capital city is Helsinki'
+// John and Mike both play basketball in different teams. In the latest 3 games,
+// John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94
+// and 123 points.
+const john_team_scors = [89, 120, 103];
+const mike_team_scors = [116, 94, 123];
 
-function describeCountry(country, population_millions, capitalCity) {
-  return `${country} has ${population_millions} million people and its capital city is ${capitalCity}`;
-}
+// 1. Calculate the average score for each team
+average = (arr) => arr.reduce((a, b) => a + b, 0) / arr.length;
 
-// 2. Call this function 3 times, with input data for 3 different
-// countries. Store the returned values in 3 different variables,
-// and log them to the console.
-const israel = describeCountry('Israel', 10, 'Jerusalem');
-const australia = describeCountry('Australia', 35, 'Canberra');
-const india = describeCountry('India', 1400, 'New Delhi');
+const john_team_avr = average(john_team_scors);
+const mike_team_avr = average(mike_team_scors);
 
-console.log(israel);
-console.log(australia);
-console.log(india);
+// 2. Decide which teams wins in average (highest average score), and print the
+// winner to the console. Also include the average score in the output.
+
+winnerAnnouncing = () => {
+  let winner = '';
+  let winner_avr = 0;
+  if (john_team_avr > mike_team_avr) {
+    winner = "Johan's team";
+    winner_avr = john_team_avr;
+  } else {
+    winner = "Mike's team";
+    winner_avr = mike_team_avr;
+  }
+
+  console.log(`${winner} wins, with arerage score of ${winner_avr}`);
+};
+
+winnerAnnouncing();
+
+// 3. Then change the scores to show different winners. Don't forget to take into
+// account there might be a draw (the same average score)
+
+// 4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105
+// points. Like before, log the average winner to the console.
+winnerAnnouncing_3 = () => {
+  let winner = '';
+  let winner_avr = 0;
+  if (john_team_avr > mike_team_avr) {
+    if (john_team_avr > mary_team_avr) {
+      winner = "Johan's team";
+      winner_avr = john_team_avr;
+    } else {
+      winner = "Mary's team";
+      winner_avr = mary_team_avr;
+    }
+  } else {
+    if (mike_team_avr > mary_team_avr) {
+      winner = "Mike's team";
+      winner_avr = mike_team_avr;
+    } else {
+      winner = "Mary's team";
+      winner_avr = mary_team_avr;
+    }
+  }
+
+  console.log(`${winner} wins, with arerage score of ${winner_avr}`);
+};
+
+// 5. Like before, change the scores to generate different winners, keeping in
+// mind there might be draws.
+const mary_team_scor = [97, 134, 105];
+const mary_team_avr = average(mary_team_scor);
+
+winnerAnnouncing_3();
