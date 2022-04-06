03bed7afc8dadef1c8b18d51a2ffa8b6903ceebf
Ofer Tauber <ofertauber@gmail.com>
2022-04-06 15:34:25 +0300

    JS -7-BOOM

--- JS/4 Order Of Operations, If Statements, Truthy and Falsy/4.4-boom/app.js
+++ JS/4 Order Of Operations, If Statements, Truthy and Falsy/4.4-boom/app.js
@@ -1,19 +1,18 @@
-// 1. Write a function called 'describeCountry' which takes three parameters:
-// 'country', 'population' and 'capitalCity'.
-// Based on this input, the function returns a string with this
-// format: 'Finland has 6 million people and its capital city is Helsinki'
+divBy7 = (num) => num % 7 === 0;
+contain7 = (num) => (num + '').split('').includes('7');
 
-function describeCountry(country, population_millions, capitalCity) {
-  return `${country} has ${population_millions} million people and its capital city is ${capitalCity}`;
-}
+boom = (n) => {
+  for (let i = 0; i <= n; i++) {
+    if (divBy7(i)) {
+      if (contain7(i)) {
+        console.log(' BOOM-BOOM,');
+      } else {
+        console.log(' BOOM,');
+      }
+    } else {
+      console.log(` ${i},`);
+    }
+  }
+};
 
-// 2. Call this function 3 times, with input data for 3 different
-// countries. Store the returned values in 3 different variables,
-// and log them to the console.
-const israel = describeCountry('Israel', 10, 'Jerusalem');
-const australia = describeCountry('Australia', 35, 'Canberra');
-const india = describeCountry('India', 1400, 'New Delhi');
-
-console.log(israel);
-console.log(australia);
-console.log(india);
+boom(20);
