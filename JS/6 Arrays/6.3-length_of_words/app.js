e099687cf6b19b0f92ced9dbb6aa82756a59d50b
Ofer Tauber <ofertauber@gmail.com>
2022-04-10 14:23:39 +0300

    Length of strings

---
+++ JS/6 Arrays/6.3-length_of_words/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Write a function that will receive an array of strings and will return a new
+// array containing all the lengths of all the strings in the array it got as a
+// parameter.
+// So that if you call this function with this array for example:
+// ["boo", "doooo", "hoo","ro"]
+// it will return this array: [3, 5, 3, 2].
+
+const stringsLen = (stringsArr) => {
+  const lengsArr = [];
+  for (const str of stringsArr) {
+    lengsArr.push(str.length);
+  }
+
+  return lengsArr;
+};
+
+console.log(stringsLen(['boo', 'doooo', 'hoo', 'ro']));
+// stringsLen(['boo', 'doooo', 'hoo', 'ro']);
