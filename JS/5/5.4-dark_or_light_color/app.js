7e9dc85ba7fae14ad8455e9b2d124f1251388281
Ofer Tauber <ofertauber@gmail.com>
2022-04-10 12:56:17 +0300

    colors

--- JS/5/5.4-dark_or_light_color/app.js
+++ JS/5/5.4-dark_or_light_color/app.js
@@ -1,1 +1,26 @@
 'use strict';
+
+// Create a function that receives one argument, a string color.
+// Create a switch statement that:
+// 1. If the color is yellow, pink or orange return from the function “light color”.
+// 2. If the color is blue, purple, brown return from the function “dark color”.
+// 3. If the color is none of the above return “Unknown color”.
+
+const darkOrLight = (color) => {
+  switch (color.toLowerCase()) {
+    case 'yellow':
+    case 'pink':
+    case 'orange':
+      return 'light color';
+    case 'blue':
+    case 'purple':
+    case 'brown':
+      return 'dark color';
+    default:
+      return 'Unknown color';
+  }
+};
+
+console.log(darkOrLight('Orange'));
+console.log(darkOrLight('BROWN'));
+console.log(darkOrLight('green'));
