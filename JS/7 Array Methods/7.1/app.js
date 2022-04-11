b46802b145f69b5255956025ab4ae4af3821fc9e
Ofer Tauber <ofertauber@gmail.com>
2022-04-11 14:57:30 +0300

    remove duplicates

--- JS/7 Array Methods/7.1/app.js
+++ JS/7 Array Methods/7.1/app.js
@@ -1,1 +1,20 @@
 'use strict';
+
+// Create a function that received an array of integers as a parameter.
+// Return a new array without any duplicates inside. You
+// indexOf as part of your solution.
+// Example:
+// [3,4,4,3,6,3] --→ [3,4,6]
+
+const arrWithDups = [3, 4, 4, 3, 6, 3, 5, 5, 1];
+
+const removeDuplicates = (arr) => {
+  const newArr = [];
+  for (const num of arr) {
+    if (-1 === newArr.indexOf(num)) newArr.push(num);
+  }
+
+  return newArr;
+};
+
+console.log(removeDuplicates(arrWithDups));
