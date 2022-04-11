f876e20ade3c2f77ce9e78d9835902ada8e19d26
Ofer Tauber <ofertauber@gmail.com>
2022-04-11 14:26:25 +0300

    object iteration

--- JS/8 Objects/8.5-object_iteration/app.js
+++ JS/8 Objects/8.5-object_iteration/app.js
@@ -1,1 +1,30 @@
 'use strict';
+
+// 1. Create a function that receives 1 argument, an object and returns a new object with the properties and values swapped.
+
+const myObj = {
+  1: 'a',
+  2: 'b',
+  3: 'c',
+};
+
+const objOpposite = (obj) => {
+  const newObj = new Object();
+  for (const prop in obj) {
+    newObj[obj[prop]] = prop;
+  }
+  // for (const key of Object.keys(obj)) {
+  //   const newKey = obj[key];
+  //   newObj[newKey] = key;
+  // }
+
+  return newObj;
+};
+
+// const myObj2 = objOpposite(myObj);
+const myObj2 = objOpposite(myObj);
+
+// myObj[1] = 'A';
+
+console.log(myObj);
+console.log(myObj2);
