22770735bf53b4218d823a6fa2ac9af7e5c1b526
Ofer Tauber <ofertauber@gmail.com>
2022-04-17 17:28:11 +0300

    debuging 2

--- JS/15/15.3.js
+++ JS/15/15.3.js
@@ -1,1 +1,10 @@
 'use strict';
+
+function getSumOfEven(arr) {
+  // return arr[2] + arr[4] + arr[6] + arr[8] + arr[10];
+  return arr[1] + arr[3] + arr[5] + arr[7] + arr[9];
+}
+getSumOfEven([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+//! In the first step I ran the function on the console and got NAN instead of 30.
+//! Using the debugger I marked a brack point in the schema operation (line 5), and set Wathc on each of the elemants - arr[10] was undefined.
