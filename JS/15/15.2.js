9f4fdcc823d8abec974d636ca3efc8d1fc9374f8
Ofer Tauber <ofertauber@gmail.com>
2022-04-17 17:18:49 +0300

    debuging 2

--- JS/15/15.2.js
+++ JS/15/15.2.js
@@ -1,1 +1,15 @@
 'use strict';
+
+function /*findSmallest*/ findSmalest(a, b, c) {
+  if (a > b > c) {
+    return c;
+  } else if (a > c > b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+
+findSmalest(52, 66, 2);
+
+//!Running the code gives an error message on the console: findSmalest is not defined. When I searched for the place where the "findSmalest" function was defined - I discovered that it was never defined, and there is a typo in the name.
