d5674ab5c8a535c90067037a93c0b13943992b65
Ofer Tauber <ofertauber@gmail.com>
2022-04-19 10:40:11 +0300

    Cars Market init

--- JS/app.js
+++ JS/app.js
@@ -1,16 +1,26 @@
 'use strict';
-String.prototype.toJadenCase = function () {
-  const arr = this.split(' ').map((word) => {
-    word = word.split('');
-    console.log(word);
-    word[0].toUpperCase();
-    console.log(word);
-    return word.join('');
-  });
+// String.prototype.toJadenCase = function () {
+//   const arr = this.split(' ').map((word) => {
+//     word = word.split('');
+//     console.log(word);
+//     word[0].toUpperCase();
+//     console.log(word);
+//     return word.join('');
+//   });
 
-  return arr;
-};
+//   return arr;
+// };
 
-var str = "How can mirrors be real if our eyes aren't real";
+// var str = "How can mirrors be real if our eyes aren't real";
 
-console.log(str.toJadenCase());
+// console.log(str.toJadenCase());
+function rgb(r, g, b) {
+  r = r > 255 ? 255 : r;
+  g = g > 255 ? 255 : g;
+  b = b > 255 ? 255 : b;
+  return r.toString(16) + g.toString(16) + b.toString(16);
+}
+console.log(rgb(0, 0, 0)); //, '000000');
+console.log(rgb(0, 0, -20)); //, '000000');
+console.log(rgb(300, 255, 255)); //, 'FFFFFF');
+console.log(rgb(173, 255, 47)); //, 'ADFF2F');
