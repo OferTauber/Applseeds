0dcbfc6a2a667e389803437d0bbe32d0ec0b46c7
Ofer Tauber <ofertauber@gmail.com>
2022-04-24 11:47:56 +0300

    DOM 1

--- JS/app.js
+++ JS/app.js
@@ -1,26 +1,82 @@
 'use strict';
-// String.prototype.toJadenCase = function () {
-//   const arr = this.split(' ').map((word) => {
-//     word = word.split('');
-//     console.log(word);
-//     word[0].toUpperCase();
-//     console.log(word);
-//     return word.join('');
-//   });
 
-//   return arr;
-// };
+var lastDigit = function (str1, str2) {
+  const lastDigStr1 = 1 * str1.charAt(str1.length - 1);
+  if (str2 === '0') return 1;
 
-// var str = "How can mirrors be real if our eyes aren't real";
+  // return Math.pow(lastDigStr1, (str2 * 1) % 12) % 10;
 
-// console.log(str.toJadenCase());
-function rgb(r, g, b) {
-  r = r > 255 ? 255 : r;
-  g = g > 255 ? 255 : g;
-  b = b > 255 ? 255 : b;
-  return r.toString(16) + g.toString(16) + b.toString(16);
-}
-console.log(rgb(0, 0, 0)); //, '000000');
-console.log(rgb(0, 0, -20)); //, '000000');
-console.log(rgb(300, 255, 255)); //, 'FFFFFF');
-console.log(rgb(173, 255, 47)); //, 'ADFF2F');
+  switch (lastDigStr1) {
+    case 0:
+      return 0;
+    case 1:
+      return 1;
+    case 2:
+      switch ((str2 * 1) % 4) {
+        case 0:
+          return 6;
+        case 1:
+          return 2;
+        case 2:
+          return 4;
+        case 3:
+          return 8;
+      }
+    case 3:
+      switch ((str2 * 1) % 4) {
+        case 0:
+          return 1;
+        case 1:
+          return 3;
+        case 2:
+          return 9;
+        case 3:
+          return 7;
+      }
+    case 4:
+      switch ((str2 * 1) % 2) {
+        case 0:
+          return 6;
+        case 1:
+          return 4;
+      }
+    case 5:
+      switch ((str2 * 1) % 2) {
+        case 0:
+          return 0;
+        case 1:
+          return 5;
+      }
+    case 6:
+      return 6;
+    case 7:
+      switch ((str2 * 1) % 4) {
+        case 0:
+          return 1;
+        case 1:
+          return 7;
+        case 2:
+          return 9;
+        case 3:
+          return 3;
+      }
+    case 8:
+      switch ((str2 * 1) % 4) {
+        case 0:
+          return 6;
+        case 1:
+          return 8;
+        case 2:
+          return 4;
+        case 3:
+          return 2;
+      }
+    case 9:
+      switch ((str2 * 1) % 2) {
+        case 0:
+          return 1;
+        case 1:
+          return 9;
+      }
+  }
+};
