236af0e357514b1921e879f5af448d6d227f1164
Ofer Tauber <ofertauber@gmail.com>
2022-04-27 13:35:46 +0300

    Build your own methods

---
+++ JS/24 prototypes/24.3Build your own methods.js
@@ -0,0 +1,52 @@
+Array.prototype.myFilter = function (callback) {
+  const toReturn = [];
+  for (let [index, element] of this.entries()) {
+    if (callback(element, index, this)) toReturn.push(element);
+  }
+
+  return toReturn;
+};
+
+Array.prototype.myFind = function (callback) {
+  for (const [index, element] of this.entries()) {
+    if (callback(element, index, this)) return element;
+  }
+
+  return;
+};
+
+Array.prototype.myReduce = function (callback, initialValue) {
+  let acc = initialValue;
+  for (let [index, value] of this.entries()) {
+    if (acc) {
+      acc = callback(acc, value, index, this);
+    } else {
+      acc = value;
+    }
+  }
+  return acc;
+};
+
+const arr = [1, 2, -2, 3, -4, 9];
+console.log(arr.myFilter(isPositive));
+console.log(arr.filter(isPositive));
+
+function isPositive(num) {
+  return num >= 0;
+}
+
+[1].reduce((i) => {
+  i + 1;
+});
+
+const nums = [1, 2, 3, 4, 5];
+console.log(
+  nums.myReduce((acc, cur, index) => {
+    return acc + cur + index;
+  }, 4)
+);
+console.log(
+  nums.reduce((acc, cur, index) => {
+    return acc + cur + index;
+  }, 4)
+);
